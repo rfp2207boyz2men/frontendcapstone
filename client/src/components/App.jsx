2024678699e0c4cc7530e5a3c9d921925f04e10f
Tony Kang <tony.andrew.kang@gmail.com>
2022-08-24 17:40:59 -0700

    Updated app.jsx to include placeholders

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { FaBeer } from 'react-icons/fa';
 import Parse from '../parse.js';
 import axios from 'axios';
+import Related from './Related.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -38,7 +39,6 @@ class App extends React.Component {
 
   }
   //https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/productsundefined
-
   //https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews:40348?count=10
   //https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=40344
   //https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=40344&count=10
@@ -49,6 +49,18 @@ class App extends React.Component {
         <div className="header">
           <div><h1>Odin <FaBeer /></h1></div>
           <div><input></input></div>
+        </div>
+        <div>
+          <>Manuel</>
+        </div>
+        <div>
+          <>Tony</>
+        </div>
+        <div>
+          <>Paul</>
+        </div>
+        <div>
+          <>Dan</>
         </div>
       </div>
     )
