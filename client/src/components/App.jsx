4130b8a52c0fc895443da74e0ae6c072ee987bfe
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-08-25 18:14:35 -0700

    Merge from dev

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,16 +2,10 @@ import React, { useState, useEffect, useContext } from 'react';
 import { FaBeer } from 'react-icons/fa';
 import Parse from '../parse.js';
 import axios from 'axios';
-<<<<<<< HEAD
-import Overview from './ProductDetail/Overview.jsx';
-import Reviews from './Reviews/Reviews.jsx';
-import { BiLoaderCircle } from 'react-icons/bi';
-=======
 import Reviews from './Reviews/Reviews.jsx';
 import { TiStarFullOutline, TiStarHalfOutline, TiStarOutline } from 'react-icons/ti';
 // import { BiLoaderCircle } from 'react-icons/bi';
 import { AtomSpinner } from 'react-epic-spinners';
->>>>>>> 16adfd8068b146ab5fe2f8e242e053cc829aad42
 // import Related from './Related.jsx';
 
 class App extends React.Component {
@@ -46,36 +40,6 @@ class App extends React.Component {
         state.reviews = reviews.data.results;
         return this.setState(state);
       })
-<<<<<<< HEAD
-
-    //If desired, can set default to the first product (which may be hardcoded)
-    // this.updateSelectedProduct(40344);
-  };
-
-  unloadComponents = (product_id) => {
-    this.setState({ loading: false }, () => this.SelectedProduct(product_id))
-  };
-
-  updateSelectedProduct = (product_id) => {
-    let state = {};
-    let params = `?product_id=${product_id}`;
-
-    Parse.getAll(`products/`, params)
-      .then((products) => {
-        let defaultIndex = Math.floor(Math.random() * products.data.length);
-        state.products = products.data;
-        state.selectedProduct = products.data[defaultIndex];
-        state.loading = true;
-        return Parse.getAll(`reviews/`, params);
-      })
-      .then((reviews) => {
-        state.reviewsData = reviews.data;
-        state.reviews = reviews.data.results;
-        return this.setState(state);
-      })
-  };
-
-=======
       .catch((err) => console.log(err));
 
     //If desired, can set default to the first product (which may be hardcoded)
@@ -122,7 +86,6 @@ class App extends React.Component {
     return stars;
   };
 
->>>>>>> 16adfd8068b146ab5fe2f8e242e053cc829aad42
   //https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=40344
   //https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=40344&count=10
 
@@ -136,10 +99,6 @@ class App extends React.Component {
             <div><input></input></div>
           </div>
           <div>
-<<<<<<< HEAD
-            <Overview selectedProduct={this.state.selectedProduct} />
-=======
->>>>>>> 16adfd8068b146ab5fe2f8e242e053cc829aad42
             <>Manuel</>
           </div>
           <div>
@@ -152,18 +111,11 @@ class App extends React.Component {
             <Reviews
               selectedProduct={this.state.selectedProduct}
               reviews={this.state.reviews}
-<<<<<<< HEAD
-            />
-          </div>
-        </div>
-        :<BiLoaderCircle />
-=======
               renderStars={this.renderStars.bind(this)}
             />
           </div>
         </div>
         :<AtomSpinner color='green' />
->>>>>>> 16adfd8068b146ab5fe2f8e242e053cc829aad42
         }
       </div>
     )
