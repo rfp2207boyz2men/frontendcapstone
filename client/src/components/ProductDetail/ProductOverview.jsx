73a4275c34ddb32a1d97d570f5650c3837f94cf5
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-08-30 14:30:05 -0700

    added share social media

--- client/src/components/ProductDetail/ProductOverview.jsx
+++ client/src/components/ProductDetail/ProductOverview.jsx
@@ -1,16 +1,26 @@
 import React, { useState, useEffect, useId } from 'react';
 import { OrbitSpinner } from 'react-epic-spinners';
-import { FaFacebookSquare, FaTwitter, FaPinterest } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaPinterest } from 'react-icons/fa';
+import { FacebookShareButton, TwitterShareButton, PinterestShareButton } from 'react-share';
 import { TiTick } from 'react-icons/ti';
 
 function ProductOverview  ({ p1, currentProduct }) {
   const [loading, setLoading] = useState(true);
+  const [shareQuote, setShareQuote] = useState(`Check this Awesome item! ${currentProduct.name}, $${currentProduct.default_price}`);
+  const [shareHashtag, setShareHashtag] = useState([`Awesome:${currentProduct.category}`, `Reviews:${currentProduct.totalReviews}`]);
+  const [shareUrl, setShareUrl] = useState(currentProduct.styles[0].photos[0].url);
 
   useEffect(() => {
     if(p1.length > 0) {
       setLoading(false);
     }
   }, [p1])
+
+  useEffect(() => {
+    setShareQuote(`Check this Awesome item! ${currentProduct.name}, only for.. $${currentProduct.default_price}`);
+    setShareHashtag(`${currentProduct.category}`);
+    setShareUrl(currentProduct.styles[0].photos[0].url);
+  }, [currentProduct])
 
   return (
     <div>
@@ -21,9 +31,15 @@ function ProductOverview  ({ p1, currentProduct }) {
         <h2>{currentProduct.slogan}</h2>
         <p>{currentProduct.description}</p>
         <div className='social'>
-        <a href="https://www.facebook.com" target='_blank'><FaFacebookSquare /></a>
-        <a href="https://www.twitter.com" target='_blank'><FaTwitter /></a>
-        <a href="https://www.pinterest.com" target='_blank'><FaPinterest /></a>
+          <FacebookShareButton url={shareUrl} quote={shareQuote} hashtag={`#${shareHashtag}`}>
+            <FaFacebook />
+          </FacebookShareButton>
+          <TwitterShareButton url={shareUrl} title={shareQuote}  hashtag={`${shareHashtag}`}>
+            <FaTwitter />
+          </TwitterShareButton>
+          <PinterestShareButton  url={shareUrl} media={shareUrl} description={shareQuote} >
+            <FaPinterest />
+          </PinterestShareButton>
         </div>
       </div>
 
